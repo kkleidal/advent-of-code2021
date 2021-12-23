4b14def1de9d1ccc8e54df548f38e4bf124f77c2
Ken Leidal <kkleidal@gmail.com>
2021-12-22 19:00:54 -0500

    part 2

--- day22/src/main.rs
+++ day22/src/main.rs
@@ -1,14 +1,26 @@
 use regex::Regex;
 use std::cmp;
-use std::collections::HashSet;
 use std::hash::{Hash, Hasher};
 use std::io;
+use std::ops::{Add, BitAnd};
 
 #[derive(Debug, Clone, Copy)]
 struct Coord {
     x: i64,
     y: i64,
     z: i64,
+}
+
+impl Add for Coord {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
 }
 
 impl PartialEq for Coord {
@@ -27,19 +39,57 @@ impl Hash for Coord {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone, Copy)]
 struct Cuboid {
     top_left: Coord,
+    bottom_right: Coord,
     size: Coord,
 }
 
-struct CuboidIterator<'a> {
-    cuboid: &'a Cuboid,
-    current: Coord,
-    exhausted: bool,
+impl PartialEq for Cuboid {
+    fn eq(&self, other: &Self) -> bool {
+        self.top_left == other.top_left && self.size == other.size
+    }
+}
+
+impl Eq for Cuboid {}
+
+impl Hash for Cuboid {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.top_left.hash(state);
+        self.size.hash(state);
+    }
 }
 
 impl Cuboid {
+    fn new(top_left: Coord, size: Coord) -> Self {
+        if size.x == 0 || size.y == 0 || size.z == 0 {
+            let origin = Coord { x: 0, y: 0, z: 0 };
+            return Self {
+                top_left: origin,
+                size: origin,
+                bottom_right: origin,
+            };
+        } else {
+            return Self {
+                top_left: top_left,
+                size: size,
+                bottom_right: top_left + size,
+            };
+        }
+    }
+
+    fn is_empty(&self) -> bool {
+        self.size.x == 0 || self.size.y == 0 || self.size.z == 0
+    }
+
+    fn area(&self) -> u64 {
+        let out: u64 = (self.size.x * self.size.y * self.size.z)
+            .try_into()
+            .expect("Invalid u64");
+        out
+    }
+
     fn intersect(&self, other: &Self) -> Self {
         let xs = cmp::max(self.top_left.x, other.top_left.x);
         let ys = cmp::max(self.top_left.y, other.top_left.y);
@@ -56,84 +106,143 @@ impl Cuboid {
             self.top_left.z + self.size.z,
             other.top_left.z + other.size.z,
         );
-        Cuboid {
-            top_left: Coord {
+        Cuboid::new(
+            Coord {
                 x: xs,
                 y: ys,
                 z: zs,
             },
-            size: Coord {
+            Coord {
                 x: cmp::max(0, xe - xs),
                 y: cmp::max(0, ye - ys),
                 z: cmp::max(0, ze - zs),
             },
-        }
+        )
     }
 
-    fn iter_coords(&self) -> CuboidIterator {
-        CuboidIterator {
-            cuboid: self,
-            current: self.top_left,
-            exhausted: false,
+    fn difference(&self, other: &Self) -> Vec<Self> {
+        let mid = self.intersect(other);
+
+        if mid.is_empty() {
+            return vec![self.clone()];
         }
+
+        let mut out: Vec<Self> = Vec::new();
+
+        if mid.top_left.x > self.top_left.x {
+            // We need to add the cuboid between the other top left x and self's top left x
+            out.push(Cuboid::new(
+                Coord {
+                    x: self.top_left.x,
+                    y: self.top_left.y,
+                    z: self.top_left.z,
+                },
+                Coord {
+                    x: mid.top_left.x - self.top_left.x,
+                    y: self.size.y,
+                    z: self.size.z,
+                },
+            ));
+        }
+        if self.bottom_right.x > mid.bottom_right.x {
+            // We need to add the cuboid between the common right x and other right x
+            out.push(Cuboid::new(
+                Coord {
+                    x: mid.bottom_right.x,
+                    y: self.top_left.y,
+                    z: self.top_left.z,
+                },
+                Coord {
+                    x: self.bottom_right.x - mid.bottom_right.x,
+                    y: self.size.y,
+                    z: self.size.z,
+                },
+            ));
+        }
+
+        // So now we're only concerned with the regions between mid.top_left.x and
+        // mid.bottom_right.x
+        if mid.top_left.y > self.top_left.y {
+            out.push(Cuboid::new(
+                Coord {
+                    x: mid.top_left.x,
+                    y: self.top_left.y,
+                    z: self.top_left.z,
+                },
+                Coord {
+                    x: mid.size.x,
+                    y: mid.top_left.y - self.top_left.y,
+                    z: self.size.z,
+                },
+            ));
+        }
+        if self.bottom_right.y > mid.bottom_right.y {
+            out.push(Cuboid::new(
+                Coord {
+                    x: mid.top_left.x,
+                    y: mid.bottom_right.y,
+                    z: self.top_left.z,
+                },
+                Coord {
+                    x: mid.size.x,
+                    y: self.bottom_right.y - mid.bottom_right.y,
+                    z: self.size.z,
+                },
+            ));
+        }
+
+        // So now we're only concerned with the regions between mid.top_left.x and
+        // mid.bottom_right.x and mid.top_left.y and mid.bottom_right.y
+        if mid.top_left.z > self.top_left.z {
+            out.push(Cuboid::new(
+                Coord {
+                    x: mid.top_left.x,
+                    y: mid.top_left.y,
+                    z: self.top_left.z,
+                },
+                Coord {
+                    x: mid.size.x,
+                    y: mid.size.y,
+                    z: mid.top_left.z - self.top_left.z,
+                },
+            ));
+        }
+        if self.bottom_right.z > mid.bottom_right.z {
+            out.push(Cuboid::new(
+                Coord {
+                    x: mid.top_left.x,
+                    y: mid.top_left.y,
+                    z: mid.bottom_right.z,
+                },
+                Coord {
+                    x: mid.size.x,
+                    y: mid.size.y,
+                    z: self.bottom_right.z - mid.bottom_right.z,
+                },
+            ));
+        }
+
+        for cube in out.iter() {
+            assert!(!cube.is_empty());
+            assert!(cube.intersect(self) == *cube);
+            assert!(cube.intersect(other).is_empty());
+        }
+        for i in 0..out.len() {
+            for j in (i + 1)..out.len() {
+                // Mutually exclusive
+                assert!(out[i].intersect(&out[j]).is_empty());
+            }
+        }
+        out
     }
 }
 
-impl Iterator for CuboidIterator<'_> {
-    type Item = Coord;
+impl BitAnd for Cuboid {
+    type Output = Self;
 
-    fn next(&mut self) -> Option<Coord> {
-        if self.exhausted {
-            return None;
-        }
-
-        if self.cuboid.size.x == 0 || self.cuboid.size.y == 0 || self.cuboid.size.z == 0 {
-            self.exhausted = true;
-            return None;
-        }
-
-        let mut carry: i64 = 1;
-        let next_x = if self.current.x + carry == self.cuboid.top_left.x + self.cuboid.size.x {
-            carry = 1;
-            self.cuboid.top_left.x
-        } else {
-            let out = self.current.x + carry;
-            carry = 0;
-            out
-        };
-        let next_y = if self.current.y + carry == self.cuboid.top_left.y + self.cuboid.size.y {
-            carry = 1;
-            self.cuboid.top_left.y
-        } else {
-            let out = self.current.y + carry;
-            carry = 0;
-            out
-        };
-        let next_z = if self.current.z + carry == self.cuboid.top_left.z + self.cuboid.size.z {
-            carry = 1;
-            self.cuboid.top_left.z
-        } else {
-            let out = self.current.z + carry;
-            carry = 0;
-            out
-        };
-
-        let out = Some(self.current.clone());
-
-        if carry > 0 {
-            self.exhausted = true;
-            return out;
-        }
-
-        self.current.x = next_x;
-        self.current.y = next_y;
-        self.current.z = next_z;
-        // println!("HERE: {:?} {:?}", self.current, self.cuboid);
-        assert!(next_x < self.cuboid.top_left.x + self.cuboid.size.x);
-        assert!(next_y < self.cuboid.top_left.y + self.cuboid.size.y);
-        assert!(next_z < self.cuboid.top_left.z + self.cuboid.size.z);
-
-        out
+    // rhs is the "right-hand side" of the expression `a & b`
+    fn bitand(self, rhs: Self) -> Self::Output {
+        self.intersect(&rhs)
     }
 }
 
@@ -166,18 +275,18 @@ impl Instruction {
             let y_end: i64 = cap.get(5).unwrap().as_str().parse().unwrap();
             let z_start: i64 = cap.get(6).unwrap().as_str().parse().unwrap();
             let z_end: i64 = cap.get(7).unwrap().as_str().parse().unwrap();
-            let cuboid = Cuboid {
-                top_left: Coord {
+            let cuboid = Cuboid::new(
+                Coord {
                     x: x_start,
                     y: y_start,
                     z: z_start,
                 },
-                size: Coord {
+                Coord {
                     x: x_end - x_start + 1,
                     y: y_end - y_start + 1,
                     z: z_end - z_start + 1,
                 },
-            };
+            );
             out.push(match cap.get(1).unwrap().as_str() {
                 "on" => Instruction::ON(cuboid),
                 "off" => Instruction::OFF(cuboid),
@@ -189,48 +298,56 @@ impl Instruction {
     }
 }
 
-fn part1(instructions: &[Instruction]) -> usize {
-    let mut currently_on: HashSet<Coord> = HashSet::new();
-    let restriction = Cuboid {
-        top_left: Coord {
-            x: -50,
-            y: -50,
-            z: -50,
-        },
-        size: Coord {
-            x: 101,
-            y: 101,
-            z: 101,
-        },
-    };
+fn count_cubes(instructions: &[Instruction], restriction: Option<Cuboid>) -> u64 {
+    let mut on_cubes: Vec<Cuboid> = Vec::new();
     for instruction in instructions {
-        // println!("---");
         match instruction {
             Instruction::ON(cuboid) => {
-                let limitted = cuboid.intersect(&restriction);
-                for coord in limitted.iter_coords() {
-                    if !currently_on.contains(&coord) {
-                        // println!("ON {:?}", coord);
-                        currently_on.insert(coord);
-                    }
+                let limitted = match restriction {
+                    Some(bounds) => cuboid.intersect(&bounds),
+                    None => cuboid.clone(),
+                };
+                let mut new_on_cubes: Vec<Cuboid> = vec![limitted];
+                for cube in on_cubes {
+                    let parts = cube.difference(&limitted);
+                    new_on_cubes.extend_from_slice(&parts[..]);
                 }
+                on_cubes = new_on_cubes;
             }
             Instruction::OFF(cuboid) => {
-                let limitted = cuboid.intersect(&restriction);
-                for coord in limitted.iter_coords() {
-                    if currently_on.contains(&coord) {
-                        // println!("OFF {:?}", coord);
-                        currently_on.remove(&coord);
-                    }
+                let limitted = match restriction {
+                    Some(bounds) => cuboid.intersect(&bounds),
+                    None => cuboid.clone(),
+                };
+                let mut new_on_cubes: Vec<Cuboid> = Vec::new();
+                for cube in on_cubes {
+                    let parts = cube.difference(&limitted);
+                    new_on_cubes.extend_from_slice(&parts[..]);
                 }
+                on_cubes = new_on_cubes;
             }
         }
     }
-    currently_on.len()
+    on_cubes.iter().map(|x| x.area()).sum()
 }
 
 fn main() {
     let instructions = Instruction::parse();
-    println!("Hello, world! {:?}", instructions);
-    println!("Part 1: {}", part1(&instructions[..]));
+    let part1_restriction = Cuboid::new(
+        Coord {
+            x: -50,
+            y: -50,
+            z: -50,
+        },
+        Coord {
+            x: 101,
+            y: 101,
+            z: 101,
+        },
+    );
+    println!(
+        "Part 1: {}",
+        count_cubes(&instructions[..], Some(part1_restriction))
+    );
+    println!("Part 2: {}", count_cubes(&instructions[..], None));
 }
